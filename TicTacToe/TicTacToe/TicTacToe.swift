adaef7ec80551f036374121264b47b4e1c50a3f1
Hamza El Hamdaoui <hamza.elhamdaoui@avantgardeit.es>
2021-06-28 17:29:40 +0200

    feat: game class

--- TicTacToe/TicTacToe/TicTacToe.swift
+++ TicTacToe/TicTacToe/TicTacToe.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+enum GameWinner {
+    case crossWin
+    case circleWin
+    case gameActive
+}
+
+enum PlayStep: Int {
+    case empty = 0
+    case cross = 1
+    case circle = 2
+}
+
+class TicTacToe {
+    
+    var player = 1
+    var status = Array(repeating: PlayStep.empty, count: 9)
+    var step = 1
+    
+    init() {
+        
+    }
+    
+    func reset() {
+        player = 1
+        status = Array(repeating: PlayStep.empty, count: 9)
+        step = 1
+    }
+    
+    func checkWinner(_ status: [PlayStep]) -> GameWinner {
+        return .gameActive
+    }
+}
